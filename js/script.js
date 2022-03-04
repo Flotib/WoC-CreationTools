75cc95fd4a69fa422f4f73260c02d7dcdf9d5c0a
Flotib <flotib@hotmail.fr>
2022-03-04 15:00:46 +0100

    Currently working B)

--- js/script.js
+++ js/script.js
@@ -2,13 +2,71 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		tab: 0, // 0 -> item; 1 -> merchants; 2 -> enemies
+		itemCreation: {
+			id: undefined,
+			name: '',
+			equipable: undefined,
+			quality: 1,
+			unique: undefined,
+			slotType: {
+				basicItem: true,
+				type: '',
+				name: undefined,
+				subtype: '',
+			},
+			icon: '',
+			baseMinDamage: undefined,
+			baseMaxDamage: undefined,
+			requiredLevel: undefined,
+        	quote: '',
+		},
 	},
 
 	watch: {},
 
-	computed: {},
+	computed: {
 
-	methods: {},
+		tabname(){
+
+			const tabMap = {
+				0: 'Item creation',
+				1: 'Merchant creation',
+				2: 'Enemy creation',
+			}
+
+			return tabMap[this.tab]
+		},
+
+		/*
+		idcheck(){
+			if 
+		},
+		*/
+
+	},
+
+	methods: {
+
+		reset(tab) {
+			if (tab == 0) {
+				this.itemCreation.id = undefined
+				this.itemCreation.name = ''
+				this.itemCreation.equipable = undefined
+				this.itemCreation.quality = 1
+				this.itemCreation.unique = undefined
+				this.itemCreation.slotType.basicItem = true
+				this.itemCreation.slotType.type = ''
+				this.itemCreation.slotType.name = undefined
+				this.itemCreation.slotType.subtype = ''
+				this.itemCreation.icon = ''
+				this.itemCreation.baseMinDamage = undefined
+				this.itemCreation.baseMaxDamage = undefined
+				this.itemCreation.requiredLevel = undefined
+				this.itemCreation.quote = ''
+			}
+		},
+
+	},
 
 	mounted() {},
 })
